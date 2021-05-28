40db268ef92f1f3128b99089e2e702424a5ec6d6
Benjamin Crawford <bcrawfor@ucsd.edu>
2021-05-28 16:03:02 -0700

    working on setup.py

--- src/ultrasonic/src/setup.py
+++ src/ultrasonic/src/setup.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
 
     print("Entering setup procedure for 1 urm13 sensor.")
     CURRENT_ADDRESS = input("Enter the current address for the sensor you want to setup (default = 0x12): ")
-    NEW_ADDRESS  = input("Enter the new address for this sensor: ")
+    NEW_ADDRESS  = input("Enter the new address for this sensor (in decimal): ")
 
     sensor = urm13(CURRENT_ADDRESS)
-    sensor.set_address(NEW_ADDRESS)
+    sensor.set_address(int(NEW_ADDRESS))
 
     RANGE_MODE = input("Enter '0' for long ranging mode and '1' for short ranging mode: ")
     sensor.config_range_mode(0)
