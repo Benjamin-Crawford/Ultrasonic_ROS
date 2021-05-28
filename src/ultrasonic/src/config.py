5cb7d5dc9f1d5a27afc90fee4992c9b416c39c85
Benjamin Crawford <bcrawfor@ucsd.edu>
2021-05-28 13:03:20 -0700

    update publisher and config

--- src/ultrasonic/src/config.py
+++ src/ultrasonic/src/config.py
@@ -4,3 +4,7 @@ URM13_ADDRESSES = [0x10,0x11,0x12,0x13]
 RANGING_MODE = 0 #0 for long ranging mode, 1 for short ranging mode
 DETECT_MODE = 1 #0 for auto detection, 1 for trigger detection
 NUM_GROUPS = 1 #defines the number of firing groups to alleviate crosstalk issues
+WAIT_TIME = 0.07 #time to wait between trigger and read
+
+#indexes of the sensors in each group leave empty if not using that group
+GROUP_MEMBERS = [[0,1,2,3]] 
--- src/ultrasonic/src/publisher.py
+++ src/ultrasonic/src/publisher.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
+from ultrasonic.src.config import NUM_GROUPS, URM13_ADDRESSES
 from urm13 import urm13
 import config
 import rospy
-from std_msgs.msg import Float64MultiArray, Bool
+from std_msgs.msg import Float64MultiArray, Time
+import time
 
 def publisher():
-    data_pub = rospy.Publisher('ultrasonic_data', Float64MultiArray, queue_size=10)
-    heart_pub = rospy.Publisher('ultrasonic_heartbeat', Bool, queue_size=10)
-    rospy.init_node('data_publisher', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    #TODO: add code to set up sensors
+    heart_pub = rospy.Publisher('ultrasonic_heartbeat', Time, queue_size=10)
+
+    rate = rospy.Rate(14) # 14hz
+    data_pubs = []
+    sensors = []
+
+    for i in range(NUM_GROUPS):
+        sensors.append([])
+        data_pubs.append(rospy.Publisher('ultrasonic_data_group_{}'.format(i), Float64MultiArray, queue_size=10))
+        rospy.init_node('data_publisher_group_{}'.format(i), anonymous=True)
+        for j in GROUP_MEMBERS[i]:
+            sensors[i].append(urm13(URM13_ADDRESSES[j]))
+
     while not rospy.is_shutdown():
-        data = [] #TODO: add code to get actual data
-        rospy.loginfo(data)
-        data_pub.publish(data)
-        heart_pub.publish(True)
+        for i in range(NUM_GROUPS):
+            distances = []
+            for sensor in sensors[i]:
+                sensor.trigger_measurement()
+            time.sleep(WAIT_TIME)
+            for sensor in sensors[i]:
+                distances.append(sensor.get_distance())
+            rospy.loginfo(distances)
+            data_pubs[i].publish(distances)
+
+        heart_pub.publish(rospy.get_rostime())
         rate.sleep()
 
 if __name__ == '__main__':
