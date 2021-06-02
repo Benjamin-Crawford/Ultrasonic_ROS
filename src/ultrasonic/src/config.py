d5ec2b275326df39062c76728aba1e5843c30715
Benjamin Crawford <bcrawfor@ucsd.edu>
2021-06-02 14:07:42 -0700

    update subscriber and config for testing

--- src/ultrasonic/src/config.py
+++ src/ultrasonic/src/config.py
@@ -3,8 +3,8 @@
 URM13_ADDRESSES = [0x10,0x11,0x12,0x13]
 RANGING_MODE = 0 #0 for long ranging mode, 1 for short ranging mode
 DETECT_MODE = 1 #0 for auto detection, 1 for trigger detection
-NUM_GROUPS = 1 #defines the number of firing groups to alleviate crosstalk issues
+NUM_GROUPS = 2 #defines the number of firing groups to alleviate crosstalk issues
 WAIT_TIME = 0.07 #time to wait between trigger and read
 
 #indexes of the sensors in each group leave empty if not using that group
-GROUP_MEMBERS = [[0,1,2,3]] 
+GROUP_MEMBERS = [[0,2][1,3]] 
--- src/ultrasonic/src/ultrasonic_subscriber.py
+++ src/ultrasonic/src/ultrasonic_subscriber.py
@@ -5,9 +5,14 @@ import config
 
 def callback(data):     
     group_num = data.data[0]
-    print("Group {}: ".format(int(group_num)),end="" )
+    if(group_num == 1.0):
+        group_name = "Front"
+    else:
+        group_name = "Back"
+    print("{} Group: ".format(group_name,end=""))
     for i, distance in enumerate(data.data[1:]):
-        print("{}: {}, ".format(int(i),distance),end="")
+        if distance < 300:
+            print("{}: {}   ".format(int(i),distance),end="")
     print("")
 
 def listener(): 
